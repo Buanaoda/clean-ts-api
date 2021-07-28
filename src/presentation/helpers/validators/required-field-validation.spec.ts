b337fe477bc9208090c6cc1887e32a97c118d73d
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-07-28 15:15:40 -0300

    test: add tests to CompareFieldsValidation

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,20 +1,26 @@
-import { MissingParamError } from '../../errors'
-import { RequiredFieldValidation } from './required-field-validation'
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
 
-const makeSut = (): RequiredFieldValidation => {
-  return new RequiredFieldValidation('field')
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
 }
 
-describe('Required Field Validation', () => {
-  test('Should return a missing param error if validation fails', async () => {
+describe('Compare Fields Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
     const sut = makeSut()
-    const error = sut.validate({ name: 'any_name' })
-    expect(error).toEqual(new MissingParamError('field'))
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'wrong_value'
+    })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
   })
 
   test('Should not return if validation succeeds', async () => {
     const sut = makeSut()
-    const error = sut.validate({ field: 'any_name' })
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'any_value'
+    })
     expect(error).toBeFalsy()
   })
 })
