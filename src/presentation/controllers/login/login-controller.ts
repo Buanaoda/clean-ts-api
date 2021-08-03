c63725e7a4dfc75623684bd8714c9a4629f1b6f4
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-08-03 10:29:11 -0300

    chore: fix eslint rules to apply semicolons

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,5 +1,5 @@
-import { Controller, HttpRequest, HttpResponse, Authentication, Validation } from './login-controller-protocols'
-import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
+import { Controller, HttpRequest, HttpResponse, Authentication, Validation } from './login-controller-protocols';
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper';
 
 export class LoginController implements Controller {
   constructor (
@@ -9,21 +9,21 @@ export class LoginController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const error = this.validation.validate(httpRequest.body)
+      const error = this.validation.validate(httpRequest.body);
       if (error) {
-        return badRequest(error)
+        return badRequest(error);
       }
-      const { email, password } = httpRequest.body
+      const { email, password } = httpRequest.body;
       const accessToken = await this.authentication.auth({
         email,
         password
-      })
+      });
       if (!accessToken) {
-        return unauthorized()
+        return unauthorized();
       }
-      return ok({ accessToken })
+      return ok({ accessToken });
     } catch (error) {
-      return serverError(error)
+      return serverError(error);
     }
   }
 }
