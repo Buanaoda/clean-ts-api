d9ae775145f60ebd18188a6640b92fde59b0dc5a
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-08-04 13:08:30 -0300

    fix: add AccountMongoRepository on DbAddAccount composition

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,6 +1,6 @@
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory';
 import { makeSignUpValidation } from './signup-validation-factory';
-import { makeAddAccount } from '../../usecases/add-account/add-account-factory';
+import { makeAddAccount } from '../../usecases/add-account/db-add-account-factory';
 import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller';
 import { Controller } from '../../../../presentation/protocols';
 import { makeLogControllerDecorator } from '../decorators/log-controller-decorator-factory';
