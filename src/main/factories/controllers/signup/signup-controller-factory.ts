49181e9e72bc7ab0d0eb4a81157679588c87bce7
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-08-04 15:42:38 -0300

    feat: ensure AddSurveyController calls validation with correct values

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,7 +1,7 @@
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory';
 import { makeSignUpValidation } from './signup-validation-factory';
 import { makeAddAccount } from '../../usecases/add-account/db-add-account-factory';
-import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller';
+import { SignUpController } from '../../../../presentation/controllers/login/signup/signup-controller';
 import { Controller } from '../../../../presentation/protocols';
 import { makeLogControllerDecorator } from '../decorators/log-controller-decorator-factory';
 
