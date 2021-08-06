9c662141d88d979fdfa21a77dca4eeda53abe188
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-08-06 13:55:26 -0300

    feat: ensure JwtAdapter verify returns correct value

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -48,4 +48,10 @@ describe('Jwt adapter', () => {
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret');
     });
   });
+
+  test('Should return a value on verify success', async () => {
+    const sut = makeSut();
+    const value = await sut.decrypt('any_id');
+    expect(value).toBe('any_value');
+  });
 });
