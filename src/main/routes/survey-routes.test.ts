ccf63d6a3437f8faef5976a3d498b44e17c76324
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-08-09 17:16:15 -0300

    refactor: move token creation to a factory helper method

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -9,6 +9,25 @@ import env from '../config/env';
 let surveyCollection: Collection;
 let accountCollection: Collection;
 
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Rodrigo',
+    email: 'Rodrigo@mail.com',
+    password: '123',
+    role: 'admin'
+  });
+  const id = res.ops[0]._id;
+  const accessToken = sign({ id }, env.jwtSecret);
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  });
+  return accessToken;
+};
+
 const makeFakeSurveyData = (): AddSurveyModel => ({
   question: 'any_question',
   answers: [
@@ -48,21 +67,7 @@ describe('Survey Routes', () => {
     });
 
     test('Should return 204 on add survey with valid token', async () => {
-      const res = await accountCollection.insertOne({
-        name: 'Rodrigo',
-        email: 'Rodrigo@mail.com',
-        password: '123',
-        role: 'admin'
-      });
-      const id = res.ops[0]._id;
-      const accessToken = sign({ id }, env.jwtSecret);
-      await accountCollection.updateOne({
-        _id: id
-      }, {
-        $set: {
-          accessToken
-        }
-      });
+      const accessToken = await makeAccessToken();
       await request(app)
         .post('/api/survey')
         .set('x-access-token', accessToken)
@@ -80,21 +85,8 @@ describe('Survey Routes', () => {
   });
 
   test('Should return 200 on load surveys with valid accessToken', async () => {
-    const res = await accountCollection.insertOne({
-      name: 'Rodrigo',
-      email: 'Rodrigo@mail.com',
-      password: '123'
-    });
     await surveyCollection.insertMany([makeFakeSurveyData(), makeFakeSurveyData()]);
-    const id = res.ops[0]._id;
-    const accessToken = sign({ id }, env.jwtSecret);
-    await accountCollection.updateOne({
-      _id: id
-    }, {
-      $set: {
-        accessToken
-      }
-    });
+    const accessToken = await makeAccessToken();
     await request(app)
       .get('/api/surveys')
       .set('x-access-token', accessToken)
