9a0ced66e00490b5fca6436ce4c8754a8abeeb00
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-08-11 11:24:00 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if its new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -1,42 +1,37 @@
 import { Collection } from 'mongodb';
-import { AddSurveyModel } from '@/domain/usecases/add-survey';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { SurveyResultMongoRepository } from './survey-result-mongo-repository';
 import { SurveyModel } from '@/domain/models/survey';
 import { AccountModel } from '@/domain/models/account';
-import { AddAccountModel } from '@/domain/usecases/add-account';
 
 let surveyCollection: Collection;
 let surveyResultCollection: Collection;
 let accountCollection: Collection;
 
-const makeFakeSurveyData = (): AddSurveyModel => ({
-  question: 'any_question',
-  answers: [
-    {
+const makeSut = (): SurveyResultMongoRepository => {
+  return new SurveyResultMongoRepository();
+};
+
+const makeSurvey = async (): Promise<SurveyModel> => {
+  const res = await surveyCollection.insertOne({
+    question: 'any_question',
+    answers: [{
       image: 'any_image',
       answer: 'any_answer'
-    },
-    {
-      answer: 'any_image'
-    }
-  ],
-  date: new Date()
-});
-
-const makeFakeAddAccountData = (): AddAccountModel => ({
-  name: 'valid_name',
-  email: 'valid_email@mail.com',
-  password: 'hashed_password'
-});
-
-const makeFakeSurvey = async (): Promise<SurveyModel> => {
-  const res = await surveyCollection.insertOne(makeFakeSurveyData());
+    }, {
+      answer: 'other_answer'
+    }],
+    date: new Date()
+  });
   return res.ops[0];
 };
 
-const makeFakeAccount = async (): Promise<AccountModel> => {
-  const res = await accountCollection.insertOne(makeFakeAddAccountData());
+const makeAccount = async (): Promise<AccountModel> => {
+  const res = await accountCollection.insertOne({
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  });
   return res.ops[0];
 };
 
@@ -50,22 +45,18 @@ describe('SurveyResultMongoRepository', () => {
   });
 
   beforeEach(async () => {
-    surveyResultCollection = await MongoHelper.getCollection('surveys');
-    await surveyResultCollection.deleteMany({});
-    surveyCollection = await MongoHelper.getCollection('surveyResults');
+    surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.deleteMany({});
+    surveyResultCollection = await MongoHelper.getCollection('surveyResults');
+    await surveyResultCollection.deleteMany({});
     accountCollection = await MongoHelper.getCollection('accounts');
     await accountCollection.deleteMany({});
   });
 
-  const makeSut = (): SurveyResultMongoRepository => {
-    return new SurveyResultMongoRepository();
-  };
-
   describe('SAVE', () => {
     test('Should add a survey result if it is new', async () => {
-      const survey = await makeFakeSurvey();
-      const account = await makeFakeAccount();
+      const survey = await makeSurvey();
+      const account = await makeAccount();
       const sut = makeSut();
       const surveyResult = await sut.save({
         surveyId: survey.id,
@@ -76,6 +67,27 @@ describe('SurveyResultMongoRepository', () => {
       expect(surveyResult).toBeTruthy();
       expect(surveyResult.id).toBeTruthy();
       expect(surveyResult.answer).toBe(survey.answers[0].answer);
+    });
+
+    test('Should update survey result if its not new', async () => {
+      const survey = await makeSurvey();
+      const account = await makeAccount();
+      const res = await surveyResultCollection.insertOne({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      });
+      const sut = makeSut();
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[1].answer,
+        date: new Date()
+      });
+      expect(surveyResult).toBeTruthy();
+      expect(surveyResult.id).toEqual(res.ops[0]._id);
+      expect(surveyResult.answer).toBe(survey.answers[1].answer);
     });
   });
 });
