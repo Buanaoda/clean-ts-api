3fad638c0416d22c8bbe24173d3c4e455334c4ee
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-08-11 10:03:05 -0300

    feat: ensure DbSaveSurveyResult returns surveyResult on success

--- src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -60,4 +60,10 @@ describe('DbAddSurvey UseCase', () => {
     const promise = sut.save(makeFakeSurveyResultData());
     await expect(promise).rejects.toThrow();
   });
+
+  test('Should return a SurveyResult on success', async () => {
+    const { sut } = makeSut();
+    const surveyResult = await sut.save(makeFakeSurveyResultData());
+    expect(surveyResult).toEqual(makeFakeSurveyResult());
+  });
 });
